10d511e87e2bfe0aa19904da5188dd1bf2791287
lavvy <lavashonline@gmail.com>
2016-05-29 10:06:15 -0700

    try again

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-wget -O /tmp/package.tar.gz http://greencreate.net/lavvy/OpenVoucher/archive/0.4.2.tar.gz
+#install relivant packages
+#apt-get install apache2 php5 php5-mysql sudo mysql-server
+
+#download openvoucher
+wget -O /tmp/package.tar.gz https://github.com/litzinetz-de/OpenVoucher/archive/0.4.2.tar.gz
 tar -zxf /tmp/package.tar.gz -C /tmp/
-cp -pr /tmp/OpenVoucher-*/src/* /var/www/
+cp -pr /tmp/OpenVoucher-*/src/* /var/www
 rm -rf /var/www/.htaccess
 
+#create mysql table for openvoucher
 mysql -uroot -e "CREATE USER 'local'@'%' IDENTIFIED BY 'local'"     
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'local'@'%' WITH GRANT OPTION"                                                                            
 #mysql -uopenvoucher -popenvoucher </app2/database/tables.sql 
