08d26fbdcd718ee1ec3a830a19fd7af984b909e5
lavvy <lavashonline@gmail.com>
2016-05-31 09:35:56 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,12 +6,12 @@
 #download openvoucher
 wget -O /tmp/package.tar.gz https://github.com/litzinetz-de/OpenVoucher/archive/0.4.2.tar.gz
 tar -zxf /tmp/package.tar.gz -C /tmp/
-cp -pr /tmp/OpenVoucher-*/src/* /var/www
-rm -rf /var/www/.htaccess
+cp -pr /tmp/OpenVoucher-*/src/* /var/www/html
+#rm -rf /var/www/.htaccess
 
 #create mysql table for openvoucher
-mysql -uroot -e "CREATE USER 'local'@'%' IDENTIFIED BY 'local'"     
-mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'local'@'%' WITH GRANT OPTION"                                                                            
+mysql -uroot -pbitnami -e "CREATE USER 'local'@'%' IDENTIFIED BY 'local'"     
+mysql -uroot -pbitnami -e "GRANT ALL PRIVILEGES ON *.* TO 'local'@'%' WITH GRANT OPTION"                                                                            
 #mysql -uopenvoucher -popenvoucher </app2/database/tables.sql 
 mysql -ulocal -plocal </tmp/OpenVoucher-*/database/tables.sql
 
